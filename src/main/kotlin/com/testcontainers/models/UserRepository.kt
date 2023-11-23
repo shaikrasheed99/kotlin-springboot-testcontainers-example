fec4de4448d9a453e3213f1c08faffaf32ee5804
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-11-23 21:18:56 +0530

    Add. Custom query to return users by email

--- src/main/kotlin/com/testcontainers/models/UserRepository.kt
+++ src/main/kotlin/com/testcontainers/models/UserRepository.kt
@@ -1,5 +1,9 @@
 package com.testcontainers.models
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface UserRepository : JpaRepository<User, String>
+interface UserRepository : JpaRepository<User, String> {
+    @Query(value = "SELECT * FROM users WHERE email = :email", nativeQuery = true)
+    fun findByEmail(email: String): List<User>
+}
