742b57ebe6c978861f52b3373e60e1cdaa7bf39a
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-11-23 21:11:58 +0530

    Add. Id annotation to user model

--- src/main/kotlin/com/testcontainers/models/User.kt
+++ src/main/kotlin/com/testcontainers/models/User.kt
@@ -1,11 +1,13 @@
 package com.testcontainers.models
 
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "users")
 data class User(
+    @Id
     private val username: String,
     private val password: String,
     private val email: String,
